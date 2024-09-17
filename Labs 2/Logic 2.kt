3e1efc896ae3b97265af40c62ed0f0fcef881624
Maohead <folzy312@gmail.com>
2024-09-17 20:37:51 +0900

    Create Logic 2.kt

---
+++ Labs 2/Logic 2.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val X = false
+    val Y = true
+    val Z = false
+
+    println("а) X или Z: ${if (X || Z) "Истина" else "Ложь"}")
+    println("б) X и Y: ${if (X && Y) "Истина" else "Ложь"}")
+    println("в) X и Z: ${if (X && Z) "Истина" else "Ложь"}")
+}
