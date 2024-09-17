18e77b6d5384977097552c9efcafba963c5904f9
Maohead <folzy312@gmail.com>
2024-09-17 20:45:23 +0900

    Update Logic 3.kt

--- Labs 2/Logic 3.kt
+++ Labs 2/Logic 3.kt
@@ -3,7 +3,7 @@ fun main() {
     val B = false
     val C = false
 
-    println("а) не A и B: ${if (!A && B) "Истина" else "Ложь"}")
-    println("б) A или не B: ${if (A || !B) "Истина" else "Ложь"}")
-    println("в) A и B или C: ${if (A && B || C) "Истина" else "Ложь"}")
+    println("а) не A и B: ${(!A && B)}")
+    println("б) A или не B: ${(A || !B)}")
+    println("в) A и B или C: ${(A && B || C)}")
 }
